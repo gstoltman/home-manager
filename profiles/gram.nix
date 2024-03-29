f6494357087b64648031a03d262fcddd1607acaa
gstoltman <gstoltman@gmail.com>
2024-03-29 10:04:20 -0700

    gram home formatting

--- profiles/gram.nix
+++ profiles/gram.nix
@@ -1,12 +1,13 @@
 { config, pkgs, inputs , ... }:
+
 {
-    home.username = "grant";
-    home.homeDirectory = "/home/grant";
+  home.username = "grant";
+  home.homeDirectory = "/home/grant";
 
-    nixpkgs.config.allowUnfreePredicate = _: true;
+  nixpkgs.config.allowUnfreePredicate = _: true;
 
-    imports = [
-      inputs.nix-colors.homeManagerModules.default
+  imports = [
+    inputs.nix-colors.homeManagerModules.default
       ./features/alacritty.nix
       ./features/bash.nix
       ./features/git.nix
@@ -15,60 +16,55 @@
       #./features/polybar.nix
       ./features/starship.nix
       ./features/vscode.nix
-    ];
+  ];
 
-    colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-storm;
+  colorScheme = inputs.nix-colors.colorSchemes.everforest;
 
-    home.packages = with pkgs; [
-	bat
-	btop
-	discord
-	firefox
-	fzf
-	neofetch
-	obsidian
-	ripgrep
-	#tmux
-	unzip
-        xclip
+  home.packages = with pkgs; [
+	  bat
+	  btop
+	  discord
+	  firefox
+	  fzf
+	  neofetch
+	  obsidian
+	  ripgrep
+	  #tmux
+	  unzip
+    xclip
 
-	### Fonts
-        ( nerdfonts.override { fonts = [ "Iosevka" ]; } )
+	  ### Fonts
+    ( nerdfonts.override { fonts = [ "Iosevka" ]; } )
 
-        ### Language Support
-        #typst
-        #go
-    ];
+    ### Language Support
+    #typst
+    #go
+  ];
 
-    home.file = {
-        #neovim = {
-        #    recursive = true;
-        #    source = ../dotfiles/nvim;
-        #    target = "./.config/nvim";
-        #};
-        #tmux = {
-        #    source = ../dotfiles/tmux/tmux.conf;
-        #    target = "./.tmux.conf";
-        #};
-    };
+  home.file = {
+    #tmux = {
+    #  source = ../dotfiles/tmux/tmux.conf;
+    #  target = "./.tmux.conf";
+    #};
+  };
 
-    home.sessionVariables = {
-        EDITOR = "nvim";
-        XDG_CACHE_HOME = "$HOME/.cache";
-        XDG_CONFIG_HOME = "$HOME/.config";
-        XDG_DATA_HOME = "$HOME/.local/share";
-        XDG_STATE_HOME = "$HOME/.local/state";
-        XDG_BIN_HOME = "$HOME/.local/bin"; 	# Not technically in the official xdg specification
-        XDG_DESKTOP_DIR="$HOME/desktop";
-        XDG_DOWNLOAD_DIR="$HOME/downloads";
-    };
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+    XDG_BIN_HOME = "$HOME/.local/bin"; 	# Not technically in the official xdg specification
+    XDG_DESKTOP_DIR="$HOME/desktop";
+    XDG_DOWNLOAD_DIR="$HOME/downloads";
+  };
 
-    home.sessionPath = [
-        "$XDG_BIN_HOME"
-    ];
+  home.sessionPath = [
+    "$XDG_BIN_HOME"
+  ];
 
-    # Restart services on switch
-    systemd.user.startServices = "sd-switch";
+  # Restart services on switch
+  systemd.user.startServices = "sd-switch";
 
 # You should not change this value, even if you update Home Manager. If you do
 # want to update the value, then make sure to first check the Home Manager
@@ -76,5 +72,5 @@
     home.stateVersion = "24.05"; # Please read the comment before changing.
 
 # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
