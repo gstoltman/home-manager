459c65465453eb8af2c7593c58c99da06f9ed8b3
Grant Stoltman <gstoltman@gmail.com>
2024-05-12 11:49:13 -0700

    gram update

--- profiles/features/kitty.nix
+++ profiles/features/kitty.nix
@@ -2,12 +2,6 @@
 {
   programs.kitty = {
     enable = true;
-
-    font = {
-      name = "ZedMono Nerd Font";
-      size = 14;
-    };
-
     settings = {
       confirm_os_window_close = 0;
     };
