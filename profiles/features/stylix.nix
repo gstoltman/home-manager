2dd247225a94a07484a620605ada1013d4c17c23
gstoltman <gstoltman@gmail.com>
2024-06-24 17:20:23 -0700

    cleaning stylix.nix

--- profiles/features/stylix.nix
+++ profiles/features/stylix.nix
@@ -3,22 +3,10 @@
 {
   stylix = {
     enable = true;
-    #wallpaper = config.lib.stylix.types.wallpaper.from.image {
-    #  file = ../../assets/everforest-wallpaper.jpg;
-    #  polarity = "dark";
-    #  };
     image = ../../assets/everforest-wallpaper.jpg;
     polarity = "dark";
     base16Scheme = ../../assets/themes/everforest.yaml;
     fonts = {
-#      serif = {
-#        package = with pkgs; (nerdfonts.override { fonts = [ "ZedMono" ]; });
-#	       name = "ZedMono Nerd Font";
-#      };
-#      sansSerif = {
-#        package = with pkgs; (nerdfonts.override { fonts = [ "ZedMono" ]; });
-#        name = "ZedMono Nerd Font";
-#      };
       monospace = {
         package = with pkgs; (nerdfonts.override { fonts = [ "ZedMono" ]; });
         name = "ZedMono Nerd Font";
